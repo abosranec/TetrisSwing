be3a91b0481839325785c741ac430063b994c05a
pasha <pasha_mugako@mail.ru>
2017-01-24 13:37:10 +0300

    start/stop game (weight mistake with start game(need coll sout...))

--- src/RunGame.java
+++ src/RunGame.java
@@ -1,5 +1,5 @@
 import mainGame.*;
-import windowSetting.MyKeyEventDispatcher;
+import windowSetting.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -7,12 +7,14 @@ import java.awt.*;
 public class RunGame implements Runnable {
     private MainFrame mainFrame;
     private GameBoard gameBoard;
+    private PanelMenu panelMenu;
     private MyKeyEventDispatcher myKeyEventDispatcher;
-    private int gameSpeed = 2000;
+    private int gameSpeed = 1000;
     private boolean gameOver = true;
     public RunGame(MainFrame mainFrame) {
         this.mainFrame = mainFrame;
         gameBoard = mainFrame.getGameBoard();
+        panelMenu = mainFrame.getPanelMenu();
         //for global keyListener
         myKeyEventDispatcher = new MyKeyEventDispatcher();
         KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(myKeyEventDispatcher);
@@ -22,49 +24,59 @@ public class RunGame implements Runnable {
     public void run() {
         while(true) {
             //start new game
-            while (gameOver) {
-                // for game speed
-                long currentTime = System.currentTimeMillis();
-                //select random figure
-                Figure currentFigure = Figure.randomFigure(gameBoard.getGridLayout());
-                gameBoard.newCurrentFigure(currentFigure);
-                //use new figure
-                while (currentFigure.isStatus()) {
-                    //move right
-                    if (myKeyEventDispatcher.isRight()) {
-                        gameBoard.moveCurrentFigure(GameBoard.RIGHT);
-                        myKeyEventDispatcher.resetRight();
+            System.out.print("");
+            if (panelMenu.isStart()) {
+                while (gameOver) {
+                    // for game speed
+                    long currentTime = System.currentTimeMillis();
+                    //select random figure
+                    Figure currentFigure = Figure.randomFigure(gameBoard.getGridLayout());
+                    gameBoard.newCurrentFigure(currentFigure);
+                    //use new figure
+                    while (currentFigure.isStatus()) {
+                        //if game start
+                        if (panelMenu.isStart()) {
+                            //move right
+                            if (myKeyEventDispatcher.isRight()) {
+                                gameBoard.moveCurrentFigure(GameBoard.RIGHT);
+                                myKeyEventDispatcher.resetRight();
+                            }
+                            //move left
+                            if (myKeyEventDispatcher.isLeft()) {
+                                gameBoard.moveCurrentFigure(GameBoard.LEFT);
+                                myKeyEventDispatcher.resetLeft();
+                            }
+                            //next figure
+                            if (myKeyEventDispatcher.isUp()) {
+                                gameBoard.moveCurrentFigure(GameBoard.NEXT);
+                                myKeyEventDispatcher.resetUp();
+                            }
+                        } else {
+                            //reset move and rotate figure
+                            myKeyEventDispatcher.resetRight();
+                            myKeyEventDispatcher.resetLeft();
+                            myKeyEventDispatcher.resetUp();
+                        }
+                        //simple move down
+                        while (Math.abs(System.currentTimeMillis() - currentTime) > myKeyEventDispatcher.setSpeed(gameSpeed)) {
+                            // for game speed
+                            currentTime = System.currentTimeMillis();
+                            //move figure for board if game start
+                            if (panelMenu.isStart())
+                                gameBoard.moveCurrentFigure(GameBoard.MOVE);
+                        }
                     }
-                    //move left
-                    if (myKeyEventDispatcher.isLeft()) {
-                        gameBoard.moveCurrentFigure(GameBoard.LEFT);
-                        myKeyEventDispatcher.resetLeft();
+                    //after destroy figure need check board on full string
+                    if (gameBoard.checkBoardOnString() == gameBoard.GAME_OVER) {
+                        gameOver = false;
+                        JOptionPane.showMessageDialog(null, "", "GAME OVER",
+                                JOptionPane.INFORMATION_MESSAGE,
+                                new ImageIcon("image\\gameOver.jpg"));
+                        continue;
                     }
-                    //next figure
-                    if (myKeyEventDispatcher.isUp()) {
-                        gameBoard.moveCurrentFigure(GameBoard.NEXT);
-                        myKeyEventDispatcher.resetUp();
-                    }
-                    //simple move down
-                    while (Math.abs(System.currentTimeMillis() - currentTime) > myKeyEventDispatcher.setSpeed(gameSpeed)) {
-                        // for game speed
-                        currentTime = System.currentTimeMillis();
-                        //move figure for board
-                        gameBoard.moveCurrentFigure(GameBoard.MOVE);
-                    }
-                }
-                //after destroy figure need check board on full string
-                if (gameBoard.checkBoardOnString() == gameBoard.GAME_OVER) {
-                    System.out.println("game over");
-                    gameOver = false;
-                    JOptionPane.showMessageDialog(null, "", "GAME OVER",
-                            JOptionPane.INFORMATION_MESSAGE,
-                            new ImageIcon("image\\gameOver.jpg"));
+
                 }
             }
-
-
-
         }
     }
 }
--- src/windowSetting/PanelMenu.java
+++ src/windowSetting/PanelMenu.java
@@ -2,27 +2,39 @@ package windowSetting;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 
 public class PanelMenu extends JPanel {
     private JButton bStart;
     private JButton bRepeat;
     private JButton bExit;
-    private boolean b;
+    private boolean start = false;
     public PanelMenu() {
         super(new GridBagLayout());
         setBorder(BorderFactory.createRaisedBevelBorder());
         init();
     }
     private void init(){
-        //create buttons
-        bStart = new JButton("старт/пауза");
+        //create buttons start/stop
+        bStart = new JButton("СТАРТ");
+        bStart.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                start = !start;
+                if(start)
+                    bStart.setText("ПАУЗА");
+                else
+                    bStart.setText("СТАРТ");
+                System.out.println(start);
+            }
+        });
         add(bStart, new GridBagConstraints(
                 0, 0, 1, 1, 0, 1,
                 GridBagConstraints.CENTER,
                 GridBagConstraints.HORIZONTAL,
                 new Insets(-15,0,-15,0), 0, 0 ));
+
+
         bRepeat = new JButton("сначала");
         add(bRepeat, new GridBagConstraints(
                 0, 1, 1, 1, 0, 1,
@@ -35,5 +47,9 @@ public class PanelMenu extends JPanel {
                 GridBagConstraints.CENTER,
                 GridBagConstraints.HORIZONTAL,
                 new Insets(-15,0,-15,0), 0, 0 ));
+    }
+
+    public boolean isStart() {
+        return start;
     }
 }
